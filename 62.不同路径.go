0d996bb7e8533185fb750da68bca56d9eb0bcf1a
lirandong <lirandong@maxrocky.com>
2023-12-10 21:42:21 +0800

    .

---
+++ 62.不同路径.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=62 lang=golang
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+func uniquePaths(m int, n int) int {
+	return 1
+}
+
+// @lc code=end
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	res := uniquePaths(3, 2)
+	fmt.Println(res)
+}
