78b849ac4e0d9abd72c94b696a0e9c7a046069fe
lirandong <lirandong@maxrocky.com>
2024-01-03 09:50:33 +0800

    1

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,7 +6,7 @@
  * @Description: In User Settings Edit
  * @FilePath: \maxrocky-foundation-pc\.prettierrc.js
  */
- module.exports = {
+module.exports = {
   printWidth: 150, // 一行的字符数，如果超过会进行换行，默认为80
   tabWidth: 2, // 一个tab代表几个空格数，默认为2
   singleQuote: true, // 字符串是否使用单引号，默认为false，使用双引号
