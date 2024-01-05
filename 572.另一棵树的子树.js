ca9390d15a9e141b7740d934bae4e811b4b2e39c
lirandong <lirandong@maxrocky.com>
2024-01-05 10:47:12 +0800

    2024年1月5日

---
+++ 572.另一棵树的子树.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=javascript
+ *
+ * [572] 另一棵树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+  function dfs(node) {
+    if (node === null) return false
+    if (node.val === subRoot.val && isSameTree(node, subRoot)) return true
+    return dfs(node.left) || dfs(node.right)
+  }
+
+  return dfs(root)
+}
+
+function isSameTree(p, q) {
+  if (!p && !q) return true
+  if (!p || !q) return false
+  if (p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
+
+// @lc code=end
