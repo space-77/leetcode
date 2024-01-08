f93be4729ea368e73587b0a2d198a829922dd557
lirandong <lirandong@maxrocky.com>
2024-01-08 11:43:29 +0800

    2024年1月8日

--- 199.二叉树的右视图.go
+++ 199.二叉树的右视图.go
@@ -22,24 +22,59 @@ type TreeNode struct {
  * }
  */
 
+// func rightSideView(root *TreeNode) (ans []int) {
+
+// 	var dfs func(node *TreeNode, depth int)
+
+// 	dfs = func(node *TreeNode, depth int) {
+// 		if node == nil {
+// 			return
+// 		}
+
+// 		// ans中还无“depth深度”的值，而第一次遍历到depth深度，故加入
+// 		if depth == len(ans) {
+// 			ans = append(ans, node.Val)
+// 		}
+// 		dfs(node.Right, depth+1)
+// 		dfs(node.Left, depth+1)
+
+//		}
+//		dfs(root, 0)
+//		return
+//	}
 func rightSideView(root *TreeNode) (ans []int) {
 
-	var dfs func(node *TreeNode, depth int)
-
-	dfs = func(node *TreeNode, depth int) {
-		if node == nil {
-			return
-		}
-
-		// ans中还无“depth深度”的值，而第一次遍历到depth深度，故加入
-		if depth == len(ans) {
-			ans = append(ans, node.Val)
-		}
-		dfs(node.Right, depth+1)
-		dfs(node.Left, depth+1)
-
+	if root == nil {
+		return
 	}
-	dfs(root, 0)
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		index := 0
+		levelLen := len(queue)
+
+		for index < levelLen {
+			index++
+			node := queue[0]
+
+			if index == levelLen {
+				ans = append(ans, node.Val)
+			}
+
+			queue = append(queue[:0], queue[1:]...)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+
+		}
+	}
+
 	return
 }
 
